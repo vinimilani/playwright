e290b099855c3f395a7f8f0ea4770c8a3efd8bb6
vinissues <viniciusmilani.bb@hotmail.com>
2024-05-20 14:54:25 -0300

    chore: first commit

---
+++ tests/deckOfCards.spec.js
@@ -0,0 +1,14 @@
+const { test, expect } = require('@playwright/test');
+
+test('Testing API - Deck of Cards', async ({ request }) => {
+    
+    const suffle = await request.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1') // 1 deck is used
+    expect(suffle.ok()).toBeTruthy();
+    expect(await suffle.json()).toHaveProperty('deck_id',);
+    const deck_id = (await suffle.json()).deck_id;
+
+    const draw = await request.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`) // draw 2 cards
+    expect(draw.ok()).toBeTruthy();
+    expect(await draw.json()).toHaveProperty('cards');
+    expect(await draw.json()).toHaveProperty('remaining', 50); // 52 - 2 = 50
+  });
